3d52f297d364f1d866f4f8376832c7cd4363d60c
Juan-dev123 <378tech@gmail.com>
2023-03-08 20:35:52 -0500

    login page was implemented

--- cypress/e2e/saucedemo.login.cy.ts
+++ cypress/e2e/saucedemo.login.cy.ts
@@ -1,25 +1,26 @@
+import { LoginPage } from "../page/index";
+
+let loginPage:LoginPage;
+
 describe("Verifying Login Process for SauceDemo", () => {
     beforeEach(() => {
         cy.visit("https://www.saucedemo.com/");
+        loginPage = new LoginPage();
     });
     
     it("Login should notify when a wrong username/password combination is used.", () => {
-    cy.get('[data-test="username"]').type("testUser");
-    cy.get('[data-test="password"]').type("testPass");
-    cy.get('[data-test="login-button"]').click();
+        loginPage.login("testUser", "testPass");
 
-    // Assertion for error message
-    cy.get("[data-test='error']").should(
-        "include.text",
-        "Username and password do not match any user in this service")
+        // Assertion for error message
+        loginPage.getErrorMsg().should(
+          "include.text",
+          "Username and password do not match any user in this service")
     });
 
     it("Login should work for existing user.", () => {
-        cy.get("[data-test='username']").type("standard_user");
-        cy.get("[data-test='password']").type("secret_sauce");
-        cy.get("[data-test='login-button']").click();
-    
-        // Assertion that verifies that products list is displayed
-        cy.get(".title").should("have.text", "Products")
+      loginPage.login("standard_user", "secret_sauce");
+
+      // Assertion that verifies that products list is displayed
+      cy.get(".title").should("have.text", "Products")
     });
 });
