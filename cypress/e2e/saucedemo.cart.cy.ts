09beb2d54c88632abb1229a452517f3cb8c5d751
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-03-15 02:04:53 -0500

    shopping cart page implemented and goToAProduct method was added in inventory.page.ts (#9)

--- cypress/e2e/saucedemo.cart.cy.ts
+++ cypress/e2e/saucedemo.cart.cy.ts
@@ -1,6 +1,8 @@
 import { InventoryPage } from "../page/index";
+import { ShoppingCartPage } from "../page/index";
 
 let inventoryPage:InventoryPage;
+let shoppingCartPage:ShoppingCartPage;
 
 describe("Verification of the addition of products in the shopping cart", () => {
     beforeEach(() => {
@@ -9,6 +11,7 @@ describe("Verification of the addition of products in the shopping cart", () => 
         cy.get("[data-test='password']").type("secret_sauce");
         cy.get("[data-test='login-button']").click();
         inventoryPage = new InventoryPage();
+        shoppingCartPage = new ShoppingCartPage();
     });
 
     it("Two added products should be displayed in the shopping cart", () => {
@@ -17,10 +20,10 @@ describe("Verification of the addition of products in the shopping cart", () => 
         inventoryPage.addProductToCart("Sauce Labs Onesie")
         inventoryPage.getShopingCartNumItems().contains('2');
         inventoryPage.goToShopingCart();
-        cy.get('.cart_list .cart_item').should('have.length', 2);
-        cy.get('#item_4_title_link .inventory_item_name').should('have.text', 'Sauce Labs Backpack');
-        cy.get('#item_2_title_link .inventory_item_name').should('have.text', 'Sauce Labs Onesie');
-        cy.get('.cart_item .cart_quantity').each(($el) => {
+        shoppingCartPage.getListOfProducts().should('have.length', 2);
+        shoppingCartPage.getNameOfItem("Sauce Labs Backpack").should('have.text', 'Sauce Labs Backpack');
+        shoppingCartPage.getNameOfItem("Sauce Labs Onesie").should('have.text', 'Sauce Labs Onesie');
+        shoppingCartPage.getQuantity().each(($el) => {
             expect($el.text()).to.equal('1')
         });
     })
